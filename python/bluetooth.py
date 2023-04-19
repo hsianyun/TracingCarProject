f24423d2c0f3fdde119ff9fe02e10d6fcc2ed396
DhgfLu <xm3ru8p@gmail.com~>
2023-04-19 20:28:48 +0800

    :]

--- python/bluetooth.py
+++ python/bluetooth.py
@@ -3,6 +3,7 @@ from BTinterface import BTinterface
 import BT
 object = BTinterface()
 object.start()
-for i in range(10000):
+for i in range(1000):
     if (object.ser.SerialReadString()):
         print(object.ser.SerialReadString())
+print('over')
